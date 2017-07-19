8884ef42ac3d9b9d324122dab7579842b8e26dbb
Ethan Breit <afishintheair@gmail.com>
2017-07-18 22:11:14 -0400

    Created Cmake Stuff

---
+++ include/com/ethanbreit/util/StrUtil.h
@@ -0,0 +1,17 @@
+//
+// Created by Ethan Breit on 2017-07-13.
+//
+
+#pragma once
+
+#include <string>
+
+namespace GE
+{
+
+    void replaceChar(std::string *, char);
+
+    int  amountOfChar(std::string str, char chr);
+
+
+}
---
+++ test/Example1/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Ethan Breit on 2017-07-13.
+//
+
+int main()
+{
+
+
+
+    return 0;
+}
