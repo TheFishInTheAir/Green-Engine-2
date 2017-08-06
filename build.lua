4935bf0680a491ff630d9c3fe1022b857218de5d
Ethan Breit <afishintheair@gmail.com>
2017-08-05 20:16:01 -0400

    Still Broke

--- build.lua
+++ build.lua
@@ -2,7 +2,7 @@ FILENAME_LENGTH = 9
 BUILD_FOLDER = ".build"
 
 
-
+is_unix = package.config:sub(1,1)=="/"
 file_path  = arg[0]
 file_dir   = file_path:sub(0, file_path:len()-FILENAME_LENGTH)
 build_dir  = file_dir .. BUILD_FOLDER
